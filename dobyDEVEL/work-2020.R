d3e145df75a18ac8149130f66d106596bbe7bab6
Søren Højsgaard <sorenh@math.aau.dk>
2021-10-11 05:32:31 +0200

    sh

--- dobyDEVEL/work-2020.R
+++ dobyDEVEL/work-2020.R
@@ -1,4 +1,33 @@
-load_all("doBy")
+document("pkg")
+load_all("pkg")
+
+
+
+f1  <- function(x, y){x + y}
+f1_ <- restrict_fun(f1, list(y=10))
+f1_
+
+summary(f1_)
+
+get_fun(f1_)
+get_restrictions(f1_)
+f1_(x=1)
+
+f2 <- function(x, y){
+  x <- x + 2 + y
+  x
+}
+f2_ <- restrict_fun(f2, list(x=1)) 
+f2_(y=1)
+
+# Notice that this is absurd:
+f2__ <- restrict_fun(f2, list(x=10), method="sub")
+f2__
+
+
+
+
+
 
 x  <- splitBy(~Treatment, CO2)
 
