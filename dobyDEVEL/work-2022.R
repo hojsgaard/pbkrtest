97b02b0ad47b3f6ccf4cf585fd0d6202bf9d8691
Søren Højsgaard <sorenh@math.aau.dk>
2023-01-03 11:43:03 +0100

    SH: SH

--- dobyDEVEL/work-2022.R
+++ dobyDEVEL/work-2022.R
@@ -1,22 +1,137 @@
 library(doBy)
 
+inv <- function(A){solve(A)}
+
+nvar <- 300
+V <- cov2cor(cor(matrix(rnorm(20000 * nvar), ncol=nvar)))
+
+args <- list(A=V)
+inv_sub <- section_fun(inv, args)
+inv_def <- section_fun(inv, args, method="def")
+inv_env <- section_fun(inv, args, method="env")
+
+microbenchmark(
+    sub=section_fun(inv, args),
+    def=section_fun(inv, args, method="def"),
+    env=section_fun(inv, args, method="env"),
+    times=5
+)
+
+library(microbenchmark)
+microbenchmark(
+    sub=inv_sub(),
+    def=inv_def(),
+    env=inv_env(),
+    times=5
+)
+
+
+
+
+
+(inv_sub() %*% V)  |> zapsmall()
+(inv_def() %*% V)  |> zapsmall()
+(inv_env() %*% V)  |> zapsmall()
 
 
 
 
 
 
-fn <- function(x)sin(x)
-ord <- 4
-x0 <- 0
-xv <- seq(-2*pi, 2*pi, 0.1)
-plot(xv, fn(xv), type="l")
-lines(xv, tay(fn, x0=x0, ord=ord)(xv), lty=2)
-abline(v=x0)
+library(doBy)
+
+
+M<-matrix(rnorm(100),nrow = 10)
+
+dput(M) |> paste0() 
+
+a<-"structure(c(1.29339876191411, -1.81546438032534, -0.0531616919995241, 
+-0.268711079463816, -0.78809966270799, -0.758257164056775, 0.320461758972395, 
+0.60718180278418, -0.468573756915408, 1.81686743162958, -0.38449538606822, 
+0.380886889724529, 0.421503577064559, -1.29526215407802, 0.791266230499853, 
+-0.207151352267576, 0.364764053389896, 0.0856228291949521, 1.43125070124485, 
+-1.06080588259864, -1.50273103080406, -0.776735157046879, -0.348504643875237, 
+-1.28739372257964, -0.96856129444401, 0.0318569763740708, -0.17162920808234, 
+-0.733030285773063, -0.983911309366191, 0.499787119586348, 0.237752850606953, 
+1.23450642163246, 0.47920405336186, -1.11475189975074, 0.384438242436412, 
+-1.63358249312713, 0.43944392386847, 0.997663435985119, 1.13063902311667, 
+-1.6290722467067, -0.00619449178359357, -0.372940074279609, -0.380250579331469, 
+-0.257379937844891, 0.324982777514084, -1.77039807276043, 0.537757419871525, 
+-1.96391917524118, -0.162639815760299, 0.654240787248413, -0.683624438499659, 
+-0.126704229379784, 0.1847046505433, -1.86060690283218, -0.176994438758492, 
+1.26594470186483, -0.671158134922846, 1.22148059118848, 0.606987149302044, 
+-1.90160522551914, -1.25357112517732, 0.17045781193866, 0.424943781687776, 
+0.578105844199386, 0.840471226048932, 0.637701428360776, -0.893835127478572, 
+-0.436111181477396, 0.205845022424686, -0.740143102850157, -1.85828693720142, 
+-1.77917426865762, -0.288322661831847, 0.598963104786103, -1.98306375615202, 
+0.170515882917581, 0.453594901981834, 0.499380214369774, 1.3256302796512, 
+1.49818485540525, -0.160080024683796, 1.13660785214363, 0.39005954450314, 
+0.128524733207781, -1.81907826184467, -1.15354234028343, -1.67650935159631, 
+0.0345195363969644, -0.309181878727179, -0.171294712420814, -0.154484889391947, 
+-1.81900584810344, 0.12804075212672, -0.133770987623703, 0.875725300780063, 
+-0.632985244190984, -1.49169721135088, -0.261231563078657, 1.46174771414603, 
+1.77668108012506), dim = c(10L, 10L))"
+
+
+f <-function (x1, x2, y, n, b1, b2) 
+{
+    matrix(c(x1 * (-n * exp(b1 * x1 + b2 * x2) + y * exp(b1 * 
+        x1 + b2 * x2) + y)/(exp(b1 * x1 + b2 * x2) + 1), x2 * 
+        (-n * exp(b1 * x1 + b2 * x2) + y * exp(b1 * x1 + b2 * 
+            x2) + y)/(exp(b1 * x1 + b2 * x2) + 1)), nrow = 2)
+}
+
+set_default(f, nms, c(2,1,2,5))
+
+nms <- c("x1", "x2", "y", "n")
+vls <- c(1,2,3,4)
+load_all("dobyDEVEL/_doby")
 
 
 
 
+f <-function (xxxxxxxxxx1, xxxxxxxxxx2, y, n, b1, b2) 
+{
+    matrix(c(xxxxxxxxxx1 * (-n * exxxxxxxxxxp(b1 * xxxxxxxxxx1 + b2 * xxxxxxxxxx2) + y * exxxxxxxxxxp(b1 * 
+        xxxxxxxxxx1 + b2 * xxxxxxxxxx2) + y)/(exxxxxxxxxxp(b1 * xxxxxxxxxx1 + b2 * xxxxxxxxxx2) + 1), xxxxxxxxxx2 * 
+        (-n * exxxxxxxxxxp(b1 * xxxxxxxxxx1 + b2 * xxxxxxxxxx2) + y * exxxxxxxxxxp(b1 * xxxxxxxxxx1 + b2 * 
+            xxxxxxxxxx2) + y)/(exxxxxxxxxxp(b1 * xxxxxxxxxx1 + b2 * xxxxxxxxxx2) + 1)), nrow = 2)
+}
+
+nms <- c("xxxxxxxxxx1", "xxxxxxxxxx2", "y", "n")
+vls <- c(1,2,3,4); vls <- vls/100000
+section_fun(f, nms, vls)()
+
+load_all("dobyDEVEL/_doby")
+
+
+
+
+
+set_default(f, nms, c(2,1,2,5))
+
+
+fun <- f
+
+
+
+set_default <- function(fun, nms, vls){
+    fmls <- formals(fun)
+    
+    i <- match(nms, names(fmls))
+    j <- c(setdiff(seq_along(fmls), i), i) # new order
+    
+    fmls <- fmls[j]
+    fmls[nms] <- vls
+    formals(fun) <- fmls
+    fun    
+}
+
+
+
+
+
+1
 
 
 
